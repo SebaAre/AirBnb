ac22e3d17f3b4fee3e717a7179c550fd8ca32489
SebaAre <arellanosebastian5@hotmail.com>
2022-11-07 20:28:56 -0300

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Nav() {
+    return (
+        <nav className="container-nav">
+            <img className="container-logo" src="./images/airbnb-logo.png"/>
+        </nav>
+    )
+} 
